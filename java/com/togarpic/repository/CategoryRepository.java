ab2893ddafb7cfa57b6688374b45d0ed28d28f4b
KiwnNguyen <nguyenduytan18.work@gmail.com>
2023-09-29 21:28:44 +0700

    định hình cấu trúc pj

---
+++ java/com/togarpic/repository/CategoryRepository.java
@@ -0,0 +1,37 @@
+package com.togarpic.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.togarpic.model.Category;
+
+@Repository
+public class CategoryRepository {
+	@Autowired
+	JdbcTemplate db;
+	
+	class CategoryRowMapper implements RowMapper<Category> {
+		@Override
+		public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Category item = new Category();
+			item.setId(rs.getInt("cat_id"));
+			item.setCat_name(rs.getString("cat_name"));
+			return item;
+		}
+	}
+	
+	public List<Category> findAll() {
+		return db.query("exec showAllCategory", new CategoryRowMapper());
+	}
+
+	public Category findById(int id) {
+		return db.queryForObject("exec showAllCategoryById ?", new CategoryRowMapper(),
+				new Object[] { id });
+	}
+}
