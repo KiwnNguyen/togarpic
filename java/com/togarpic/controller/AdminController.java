22f46c2174610bb7e1463ceeacd2da9ada4cf570
Cookmeal09 <Cookmeal09@gmail.com>
2023-10-20 09:29:14 +0700

    something
    is wrong

--- java/com/togarpic/controller/AdminController.java
+++ java/com/togarpic/controller/AdminController.java
@@ -426,6 +426,254 @@ public class AdminController implements WebMvcConfigurer {
 
 	// INSERT ACTION
 
+<<<<<<< Updated upstream
+=======
+	@RequestMapping(value = "/insert1", method = RequestMethod.GET)
+		public String insertorder(Model model) {
+			Iterable<UserDB> usr = usr1.findAll1();
+			model.addAttribute("listUsrid", usr);
+			return "admin/order/insert_order";
+
+		}
+
+		@RequestMapping(value = "/insert1submit", method = RequestMethod.POST)
+		public String InsertOrder(Model model, @RequestParam("usrid") long userid, Order Order) {
+			try {
+				Order.setUsr_id(userid);
+				//Order.setUsr_id1(userid);
+				ord1.insert(Order);
+				//Show thông tin bảng order sau khi insert
+				Iterable<Order> ord = ord1.findAll1();
+				model.addAttribute("listOrder", ord);
+				//show thông tin bảng ord_details sau khi insert
+				Iterable<Orderdetails> ord_details = ord_det1.findAll1();
+				model.addAttribute("listOrderDetails", ord_details);
+				return "redirect:/admin/table";
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error value insert!!");
+			}
+		}
+
+		@RequestMapping(value = "/insertorddetail", method = RequestMethod.GET)
+		public String insertorderdetails(Model model) {
+			Iterable<Order> ord = ord1.findAll1();
+			model.addAttribute("listOrder", ord);
+	
+			Iterable<StorageView> sto = sto1.findAll1();
+			model.addAttribute("listSto", sto);
+			return "admin/order_details/insert_order_details";
+
+		}
+
+		// ----------------------------------------------------------
+		@RequestMapping(value = "/insert2submit", method = RequestMethod.POST)
+		public String insertrec(Model model, @RequestParam("ordid") long ordid, @RequestParam("stoid") long stoid,
+				@RequestParam("quantity") int quantity, @RequestParam("importprice") float importprice,
+				@RequestParam("exportprice") float exportprice, Orderdetails Order_dt) {
+
+			try {
+				Order_dt.setOrd_id(ordid);
+				Order_dt.setSto_id(stoid);
+				Order_dt.setOdt_quantity(quantity);
+				Order_dt.setOdt_importPrice(importprice);
+				Order_dt.setOdt_exportPrice(exportprice);
+
+				ord_det1.insert(Order_dt);
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error submit insert!!");
+			}
+			Iterable<Order> ord = ord1.findAll1();
+			model.addAttribute("listOrder", ord);
+
+			Iterable<Orderdetails> ord_details = ord_det1.findAll1();
+			model.addAttribute("listOrderDetails", ord_details);
+
+			return "redirect:/admin/table";
+
+		}
+
+		
+
+		@RequestMapping(value = "/insert3submit", method = RequestMethod.POST)
+		public String Insertreviewpost(Model model, @RequestParam("userid") long userid, @RequestParam("odtid") long odtid,
+				@RequestParam("proid") long proid, @RequestParam("revcon") String revcon, Review Review) {
+			try {
+
+				Review.setUsr_id(userid);
+				Review.setOdt_id(odtid);
+				Review.setPro_id(proid);
+				Review.setRev_content(revcon);
+
+				rev1.insert(Review);
+
+				Iterable<Order> ord = ord1.findAll1();
+				model.addAttribute("listOrder", ord);
+
+				Iterable<Orderdetails> ord_details = ord_det1.findAll1();
+				model.addAttribute("listOrderDetails", ord_details);
+
+				Iterable<Review> review = rev1.findAll1();
+				model.addAttribute("listreview", review);
+
+				return "redirect:/admin/table";
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error value insert!!");
+			}
+
+		}
+
+		
+
+	//SHOW INSERT CART
+	@RequestMapping(value = "/insertCart", method = RequestMethod.GET)
+	public String insertcart(Model model) {
+			model.addAttribute("Cart", car1.findAll1());
+			return "admin/cart/insert";
+
+		}
+
+	@RequestMapping(value = "/insertCartSubmit", method = RequestMethod.POST)
+	public String insertcart(Model model, 
+			@RequestParam("usr_id") long usr_id, Cart cart) {
+			try {
+				cart.setUsr_id(usr_id);
+				car1.insert(cart);
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error submit cart!!");
+			}
+			Iterable<Cart> car = car1.findAll1();
+			model.addAttribute("listCart", car);
+			return "redirect:/admin/alltable";
+
+		}
+	
+	//SHOW INSERT CART ITEM
+	@RequestMapping(value = "/insertCartItem", method = RequestMethod.GET)
+	public String insertcartitem(Model model) {
+			return "admin/cartItem/insert";
+		}
+
+	@RequestMapping(value = "/insertCartItemSubmit", method = RequestMethod.POST)
+	public String insertcartitemsubmit(Model model, @RequestParam("car_id") long car_id, @RequestParam("pro_id") long pro_id,
+				@RequestParam("cai_quantity") float cai_quantity, CartItem cartitem) {
+
+			try {
+				cartitem.setCar_id(car_id);
+				cartitem.setPro_id(pro_id);
+				cartitem.setCai_quantity(cai_quantity);
+				carItem1.insert(cartitem);
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error submit cartitem!!");
+			}
+			Iterable<CartItem> carItem = carItem1.findAll2();
+			model.addAttribute("listCartItem", carItem);
+			return "redirect:/admin/table";
+
+		}
+
+		@RequestMapping(value = "/searchInfo", method = RequestMethod.POST)
+		//@PostMapping("/searchInfo")
+		public String Showinfo(@RequestParam("info") String info,Model model) {
+			
+			if(info.equals("table order")) {
+				Iterable<Order> ord = ord1.findAll1();
+				model.addAttribute("listOrder", ord);
+				return "admin/order/tableBasic";
+			}else if(info.equals("table order details") ) {
+				Iterable<Orderdetails> ord_details = ord_det1.findAll1();
+				model.addAttribute("listOrderDetails", ord_details);
+				return "admin/order_details/tableBasic";
+			}else if(info.equals("table all")) {
+				return "redirect:/admin/table";
+			}else if(info.equals( "table review")){
+				Iterable<Review> review = rev1.findAll1();
+				model.addAttribute("listreview", review);
+				return "admin/review/tableBasic";
+			}
+			return "admin/dashboard";
+		
+		}
+
+	// SHOW INSERT FORM
+	@RequestMapping(value = "/insertUser", method = RequestMethod.GET)
+	public String insertUser(Model model) {
+		try {
+
+			MyUploadForm myUploadForm2 = new MyUploadForm();
+			model.addAttribute("myUploadForm", myUploadForm2);
+			return "/admin/user/insert";
+			      
+		}catch(Exception ec) {
+		  	ec.printStackTrace();
+			throw new RuntimeException("Error in page insert!!");
+		}
+	}
+
+	@RequestMapping(value = "/insertUserSubmit", method = RequestMethod.POST)
+	public String InsertUserSubmit(Model model,@RequestParam("firstName") String firstName,
+		@RequestParam("lastName")String lastName,@RequestParam("telephone") String telephone,
+		@RequestParam("email")String email,@RequestParam("image") String image,
+		@RequestParam("password")String password,
+		@RequestParam("role") int role,@RequestParam("fileDatas") MultipartFile file1
+		,MyUploadForm myUploadForm,
+		@ModelAttribute("myUploadForm") MyUploadForm myUploadForm1,
+		HttpServletRequest request, User user) {
+		try {
+			user.setUsr_firstName(firstName);
+			user.setUsr_lastName(lastName);
+			user.setUsr_telephone(telephone);
+			user.setUsr_email(email);
+			user.setUsr_image(image);
+			user.setUsr_password(password);
+			user.setUsr_role(role);
+				
+			usr1.insert(user);
+
+			Path staticPath = Paths.get("src", "main", "resources", "static","image");
+			String usr1 = staticPath.toString();
+			File uploadRootDir1 = new File(usr1);
+			if (!uploadRootDir1.exists()) {
+			 	uploadRootDir1.mkdirs();
+			}
+			MultipartFile[] fileDatas = myUploadForm.getFileDatas();
+			List<File> uploadedFiles = new ArrayList<File>();
+			for (MultipartFile fileData : fileDatas) {
+				 		 //Lấy tên ảnh
+				String originalFilename = fileData.getOriginalFilename();
+				try {
+				 			 // Đường dẫn static + tên đường dẫn ảnh
+				 	File serverFile = new File(uploadRootDir1.getAbsolutePath() + File.separator + originalFilename);
+			 			
+				 	BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+							 
+					stream.write(fileData.getBytes());
+				    stream.close();
+						 
+				    uploadedFiles.add(serverFile);
+				 			
+				}catch(Exception ex) {
+				 			 
+				}
+				 		 
+			}
+				
+			} catch (Exception ec) {
+				ec.printStackTrace();
+				throw new RuntimeException("Error value insert!!");
+			}
+			MultipartFile[] fileDatas = myUploadForm.getFileDatas();
+			Iterable<User> usr = usr1.findAll();
+			model.addAttribute("listOrder", usr);
+			return "redirect:/admin/alltable";
+		}
+
+	// Category
+>>>>>>> Stashed changes
 	@RequestMapping(value = "/insCategory", method = RequestMethod.GET)
 	public String showInsertCategory() {
 
--- java/com/togarpic/repository/StorageRepository.java
+++ java/com/togarpic/repository/StorageRepository.java
@@ -42,12 +42,63 @@ public class StorageRepository {
 			}
 		}
 	}
+<<<<<<< Updated upstream
 		public List<Storage> findAll() {
 			return db.query("exec showAllStorage", new StorageRowMapper());
+=======
+
+	public List<Storage> findAll() {
+		return db.query("exec showAllStorage", new StorageRowMapper());
+	}
+
+	public Storage findById(int id) {
+		return db.queryForObject("exec showStorageById ?", new StorageRowMapper(), new Object[] { id });
+	}
+
+	public int insert(Storage storage) {
+		return db.update("exec insertStorage ?,?,?",
+			new Object[] { storage.getPro_id(), storage.getSto_price(), storage.getSto_quantity() });
+	}
+
+	public long deleteById(long id) {
+		return db.update("exec deleteStorage ?", new Object[] { id });
+	}
+
+	public int update(Storage storage) {
+		return db.update("exec updateStorage ?, ?, ?, ?", new Object[] { storage.getPro_id(),
+			storage.getSto_price(), storage.getSto_quantity(), storage.getSto_id() });
+	}
+
+	class StorageViewRowMapper implements RowMapper<StorageView> {
+
+		@Override
+		public StorageView mapRow(ResultSet rs, int rowNum) throws SQLException {
+			try {
+				StorageView item = new StorageView();
+				item.setPro_id(rs.getInt("pro_id"));
+				item.setSto_date(rs.getTimestamp("sto_date"));
+				item.setSto_id(rs.getInt("sto_id"));
+				item.setSto_price(rs.getFloat("sto_price"));
+				item.setSto_quantity(rs.getInt("sto_quantity"));
+				item.setPro_name(rs.getString("pro_name"));
+				return item;
+			} catch (SQLException e) {
+				throw e;
+			}
+>>>>>>> Stashed changes
 		}
 
+<<<<<<< Updated upstream
 		public Storage findById(int id) {
 			return db.queryForObject("exec showStorageById ?", new StorageRowMapper(), new Object[] { id });
+=======
+	public List<StorageView> findAll1() {
+		try {
+			return db.query("exec showAllStorageName", new StorageViewRowMapper());
+		}catch(Exception ec){
+			ec.printStackTrace();
+			throw new RuntimeException("Error!!");	
+>>>>>>> Stashed changes
 		}
 
 		public int insert(Storage storage) {
